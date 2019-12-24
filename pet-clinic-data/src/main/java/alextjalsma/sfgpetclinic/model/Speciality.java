d84719ca9e2c07ad40e6a01e1e02dd04b5181c4d
atjalsma@home.nl <atjalsma@home.nl>
2019-12-24 11:47:03 +0100

    added Speciality entity

---
+++ pet-clinic-data/src/main/java/alextjalsma/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package alextjalsma.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String descreption;
+
+    public String getDescreption() {
+        return descreption;
+    }
+
+    public void setDescreption(String descreption) {
+        this.descreption = descreption;
+    }
+}
