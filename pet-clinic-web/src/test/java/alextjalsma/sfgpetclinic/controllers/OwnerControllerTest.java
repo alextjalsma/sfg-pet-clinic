b8b1a3d5c916c771832108d6d4679001d5271f44
atjalsma@home.nl <atjalsma@home.nl>
2020-01-08 20:25:08 +0100

    force for Circleci

--- pet-clinic-web/src/test/java/alextjalsma/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/alextjalsma/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -20,6 +20,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**
+ * test OwnerController
+ */
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
