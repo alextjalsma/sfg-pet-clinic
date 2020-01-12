7cd3e605f42bbb3fb7cccde30207f10d3d412ad0
atjalsma@home.nl <atjalsma@home.nl>
2020-01-12 16:55:19 +0100

    added findOwners

--- pet-clinic-data/src/main/java/alextjalsma/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/alextjalsma/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package alextjalsma.sfgpetclinic.repositories;
 import alextjalsma.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/alextjalsma/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/alextjalsma/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package alextjalsma.sfgpetclinic.services;
 
 import alextjalsma.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
